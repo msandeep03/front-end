6822cd9b84bfafaf32d3da87616050d21efc07a7
Anja Ishmukhametova <miukki@gmail.com>
2019-01-09 16:16:47 +0800

    prepare store route

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { Switch }           from 'react-router-dom'
 
 
 // import RouterAppDefault     from 'app/default/page/web/Router'
-import RouterAppHoney       from 'app/honey/page/web/Router'
+// import RouterAppHoney       from 'app/honey/page/web/Router'
 // import RouterAppPolyvore    from 'app/polyvore/page/web/Router'
 
 import GlobalStyle          from 'evoke-me/page/web/GlobalStyle'
 import ProviderSpace        from 'evoke-me/space/all/Provider'
 import { optionSet }        from 'evoke-me/space/api/manager'
+
+import PageHome             from 'app/honey/page/web/PageHome'
+import PageStore             from 'app/honey/page/web/PageStore'
+import PageProductList             from 'app/honey/page/web/PageProductList'
 
 
 optionSet({ HMR: true })
@@ -23,7 +27,9 @@ class App extends Component {
         <GlobalStyle />
         <BrowserRouter>
           <Switch>
-            <Route exact path='/*' component={RouterAppHoney} />
+            <Route exact path='/' component={PageHome} />
+            <Route exact path='/store' component={PageStore} />
+            <Route exact path='/products' component={PageProductList} />
             {/* <Route exact path='/app.polyvore*' component={RouterAppPolyvore} />
             <Route exact path='/*' component={RouterAppDefault} /> */}
           </Switch>
